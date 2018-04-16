32c716d3c71802556cca9d99e5f42e2211a86a4a
lzw <1129459766@qq.com>
2018-04-16 14:24:47 +0800

    asd

--- main.c
+++ main.c
@@ -73,7 +73,7 @@ int process_airkiss(const unsigned char *packet, int size)
         LOG_TRACE("Airkiss completed.");
         airkiss_get_result(akcontex, &ak_result);
         LOG_TRACE("Result:\nssid:[%s] \n ssid_crc:[%x]\nkey_len:[%d]\nkey:[%s]\nrandom:[0x%02x]",
-            ak_result.ssid
+            ak_result.ssid,
             ak_result.reserved,
             ak_result.pwd_length,
             ak_result.pwd,
